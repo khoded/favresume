4a305426433dd32789a5147a74f76c32880e7a60
korede <korede@yahoo.com>
2020-06-07 10:07:16 +0100

    -integrated google drive to frontend -frontend fix on icons

--- routes/home.js
+++ routes/home.js
@@ -48,59 +48,83 @@ router.post("/generate", (req, res) => {
   var html = template({ content: webpage });
   var filename = `${data.firstname}${
     data.lastname
-  }${new Date().toLocaleDateString()}`;
+  }`;
   //create PDF from the above generated html
   pdf
     .create(html, options)
     .toFile(`./public/${filename}.pdf`, function(err, resp) {
+      console.log(resp);
       if (resp) return res.json({ filename: filename + ".pdf" });
       if (err) return console.log(err);
-      console.log(res);
-    });
+ });
 });
 
 router.post("/upload", function(req, res) {
-  // not auth
-  if (!req.user) res.redirect("/auth/login/google");
-  else {
-    // auth user
+  var data = req.body;
+  var webpage = page.pdfBody(data);
+  //setting options for PDF
+  var options = { format: "A4" };
 
-    // config google drive with client token
-    const oauth2Client = new google.auth.OAuth2();
-    oauth2Client.setCredentials({
-      access_token: req.user.accessToken
-    });
-
-    const drive = google.drive({
-      version: "v3",
-      auth: oauth2Client
-    });
-
-    //move file to google drive
-
-    let { name: filename, mimetype, data } = req.files.file_upload;
-
-    const driveResponse = drive.files.create({
-      requestBody: {
-        name: filename,
-        mimeType: mimetype
-      },
-      media: {
-        mimeType: mimetype,
-        body: Buffer.from(data).toString()
+  //Reads the Base Template from the Views Folder
+  var template = hbs.compile(fs.readFileSync("./views/gen.hbs", "utf8"));
+  //Proccessing the base template with the content
+  var html = template({ content: webpage });
+  var filename = `${data.firstname}${
+    data.lastname
+  }`;
+  //create PDF from the above generated html
+  pdf
+    .create(html, options)
+    .toFile(`./public/${filename}.pdf`, function(err, resp) {30
+      if (!req.user) res.redirect("/auth/login/google");
+      else {
+        // auth user
+    
+        // config google drive with client token
+        const oauth2Client = new google.auth.OAuth2();
+        oauth2Client.setCredentials({
+          access_token: req.user.accessToken
+        });
+    
+        const drive = google.drive({
+          version: "v3",
+          auth: oauth2Client
+        });
+    
+        //move file to google drive
+    
+     //   let  data  = fs.createReadStream(`./public/${filename}.pdf`);
+       
+    
+        const driveResponse = drive.files.create({
+          requestBody: {
+            name: `${filename}.pdf`,
+            mimeType: "application/pdf"
+          },
+          media: {
+            mimeType: "application/pdf",
+            body: fs.createReadStream(`./public/${filename}.pdf`)
+          }
+        });
+    
+        driveResponse
+          .then(data => {
+            if (data.status == 200){ 
+              console.log("uploaded") 
+              res.redirect("/editor?file=upload");
+            }
+            
+            // success
+            else{
+              console.log("ouch") 
+               res.redirect("/editor?file=notupload");
+             } // unsuccess
+          })
+          .catch(err => {
+            throw new Error(err);
+          });
       }
     });
-
-    driveResponse
-      .then(data => {
-        if (data.status == 200) res.redirect("/dashboard?file=upload");
-        // success
-        else res.redirect("/dashboard?file=notupload"); // unsuccess
-      })
-      .catch(err => {
-        throw new Error(err);
-      });
-  }
 });
 
 module.exports = router;
