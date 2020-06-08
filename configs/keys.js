57e9c202a01546019eda9f8b7111868816246e07
Okikioluwa Onamade <khodedng@yahoo.com>
2020-06-08 21:41:53 +0100

    Update keys.js

--- configs/keys.js
+++ configs/keys.js
@@ -1,7 +1,7 @@
 const KEYS = {
     'googleOauth': {
-        'clientID': "948103666603-qj71gukhm51805vof2861q3huuu1uetg.apps.googleusercontent.com",
-        'clientSecret': "M61wWOyIJvJzq-3LRjQt277b",
+         'clientID': "a1tbu2bb0a9mri2uuspn6047pv60jp0l.apps.googleusercontent.com",
+        'clientSecret': "j9x2Vtf8uEnq6ACnnxi6Tepg",
         'callback': '/auth/google/redirect'
     },
     "session_key": "secret123"
